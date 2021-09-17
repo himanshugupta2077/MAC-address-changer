71d964f7de187634a288ec68d17c3f90d4046f99
zexceed012 <zexceed020@protonmail.com>
2021-09-17 08:00:06 +0530

    Basic MAC changer

--- 001.py
+++ 001.py
@@ -2,4 +2,14 @@
 
 import subprocess
 
+print('Below is the list of Network Interfaces in your device with their properties listed below them: \n')
+
 subprocess.call("ifconfig", shell=True)
+
+interface = input("Enter the network interface you want to change name of: ")
+new_MAC = input("Enter new MAC address for the selected inteface: ")
+
+subprocess.run(f"ifconfig {interface} down", shell=True)
+subprocess.run(f"ifconfig {interface} hw ether {new_MAC}", shell=True)
+subprocess.run(f"ifconfig {interface} up", shell=True)
+
