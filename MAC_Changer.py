a81b6112cc25d66324dcd414392b9477f50e1f69
zexceed012 <zexceed020@protonmail.com>
2021-09-18 14:13:36 +0530

    formality

--- MAC_Changer.py
+++ MAC_Changer.py
@@ -6,8 +6,10 @@ print('\nBelow is the list of Network Interfaces in your device with their prope
 
 subprocess.call("ifconfig", shell=True)
 
-interface = input("Enter the name of desired Network Interface: ")
-new_MAC = input(f"\nEnter new MAC address for {interface}: ")
+interface = input("Enter Network Interface > ")
+new_MAC = input(f"\nNew MAC address for {interface} > ")
+
+print(f"Changing MAC Adress of {interface} to {new_MAC}")
 
 subprocess.run(f"ifconfig {interface} down", shell=True)
 subprocess.run(f"ifconfig {interface} hw ether {new_MAC}", shell=True)
@@ -18,4 +20,4 @@ check = input("\nTo check the MAC address press c:\nTo quit press q:\n")
 if check == 'c':
 	subprocess.call("ifconfig", shell=True)
 elif check == 'q':
-	exit
+	exit()
